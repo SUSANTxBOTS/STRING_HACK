b54ffde8c9e1f926d58d767d7a1d4b935dffcf7d
SUSANTxBOTS <susantpc1232@gmail.com>
2025-10-18 23:50:06 +0530

    Update config.py

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ class Config:
     API_ID = "21134445"
     API_HASH = "231c18ea7273824491d6bf05425ab74e"
     #TOKEN = "6521122303:AAGCO3XMjcA0SN5NAi1M0NpmbmMxEtwwYbg"
-    TOKEN = os.environ.get("TOKEN", None)
-    MONGO_URL = "mongodb+srv://vivek:1234567890@cluster0.c48d8ih.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
-    START_PIC = ""
-    SUDOERS = filters.user(["8143754205"])
+    TOKEN = "8327719178:AAG0hk38Ad9n1exOucvECCgzEGjH_VcI5sk"
+    MONGO_URL = "mongodb+srv://iamnobita1:nobitamusic1@cluster0.k08op.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+    START_PIC = "https://ibb.co/DPNR5X8W"
+    SUDOERS = filters.user(["8156708830"])
