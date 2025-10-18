6739444baf770cf5eacc7b0ae5ab073a3d350cd5
SUSANTxBOTS <susantpc1232@gmail.com>
2025-10-18 22:56:07 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,10 +3,10 @@ from pyrogram import filters
 import os
 
 class Config:
-    API_ID = "27079591"
-    API_HASH = "c81ae4c3dc026ea4bf49842a8ce4a5f9"
+    API_ID = "21134445"
+    API_HASH = "231c18ea7273824491d6bf05425ab74e"
     #TOKEN = "6521122303:AAGCO3XMjcA0SN5NAi1M0NpmbmMxEtwwYbg"
     TOKEN = os.environ.get("TOKEN", None)
     MONGO_URL = "mongodb+srv://vivek:1234567890@cluster0.c48d8ih.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
-    START_PIC = "https://files.catbox.moe/ppvvg0.jpg"
+    START_PIC = ""
     SUDOERS = filters.user(["8143754205"])
